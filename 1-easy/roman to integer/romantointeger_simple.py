758f956116d75b56cf1f5156f9941baddab36135
Murilo Lugli <murilolugli1@gmail.com>
2024-04-03 11:17:57 -0300

    rti

---
+++ 1-easy/roman to integer/romantointeger_simple.py
@@ -0,0 +1,20 @@
+#just trying to accomplish the objective (not worrying about getting it right on leetcode or making the code readable)
+romanNumber = str(input('Type any roman number between 1 to 3999\nExample: "CM"   '))
+romanNumber_list = list(romanNumber)
+print(romanNumber_list)
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+
+
+    
+
+
+
+
